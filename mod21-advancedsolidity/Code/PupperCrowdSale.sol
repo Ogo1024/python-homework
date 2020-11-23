096eadc83ba93eb33f55edf387350a12e5501235
Ogo1024 <bluesuncap@gmail.com>
2020-11-23 17:40:38 -0500

    file update

--- mod21-advancedsolidity/Code/PupperCrowdSale.sol
+++ mod21-advancedsolidity/Code/PupperCrowdSale.sol
@@ -1,18 +1,66 @@
 pragma solidity ^0.5.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+import "./PupperCoin.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
-contract PupperCoin is ERC20, ERC20Detailed, ERC20Mintable {
+// @TODO: Inherit the crowdsale contracts
+contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
+    
+    uint _rate = 1;
+
     constructor(
-        string memory name,
-        string memory symbol,
-        uint initial_supply
+        // @TODO: Fill in the constructor parameters!
+        uint rate,
+        address payable wallet,
+        PupperCoin token, 
+        uint goal, 
+        uint256 open, 
+        uint256 close
     )
-        ERC20Detailed(name, symbol, 18)
+        // @TODO: Pass the constructor parameters to the crowdsale contracts.
+        Crowdsale(rate, wallet, token)
+        MintedCrowdsale()
+        CappedCrowdsale(goal)
+        TimedCrowdsale(open, close)
+        PostDeliveryCrowdsale()
+        RefundableCrowdsale(goal)
+        
         public
     {
         // constructor can stay empty
+    }
+}
+
+contract PupperCoinCrowdsaleDeployer {
+
+    address public token_sale_address;
+    address public token_address;
+
+    constructor(
+        // @TODO: Fill in the constructor parameters!
+        string memory name,
+        string memory symbol, 
+        address payable wallet, 
+        uint goal
+        
+    )
+        public
+    {
+        // @TODO: create the PupperCoin and keep its address handy
+        PupperCoin token = new PupperCoin(name, symbol, 0);
+        token_address = address(token);
+
+        // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+        PupperCoinSale token_sale = new PupperCoinSale(1, wallet, token, goal, now, now + 24 weeks);
+            
+        token_sale_address = address(token_sale);
+            
+        // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
+        token.addMinter(token_sale_address);
+        token.renounceMinter();
     }
 }
